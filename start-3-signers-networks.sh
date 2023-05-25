5fba96c441114faa61f70df87f4e0fc82c8a7966
Daniel Liu <liudaniel@qq.com>
2023-05-25 12:03:49 +0800

    start or stop any node

--- start-3-signers-networks.sh
+++ start-3-signers-networks.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-_interupt() { 
-    echo "Shutdown ${CHILD_PIDS}"
-    kill -TERM ${CHILD_PIDS}
-    exit
-}
+set -eo pipefail
 
-trap _interupt INT TERM
+if [ $# == 0 ] ; then
+    echo "Usage: $0 node_id ..."
+    echo "Examples:"
+    echo "$0 0"
+    echo "$0 2"
+    echo "$0 0 1"
+    echo "$0 2 3"
+    echo "$0 0 1 2"
+    echo "$0 0 1 2 3"
+    exit 1
+fi
+
+for arg in $@; do
+    if ! [ ${arg} -ge 0 ] 2>/dev/null ; then
+        echo "node_id ${arg} is not integer"
+        exit 2
+    fi
+done
 
 # Logging verbosity: 0=silent, 1=error, 2=warn, 3=info, 4=debug, 5=detail (default: 3)
 VERBOSITY=4
@@ -16,75 +29,94 @@ BASE_RPC_PORT=8545
 BASE_WS_RPC_PORT=9545
 
 WORK_DIR=${PWD}
-LOG_DIR="${WORK_DIR}/logs"
+LOG_DIR="logs"
 PROJECT_DIR="${HOME}/XDPoSChain"
+BOOTNODE_PID_FILE="bootnode.pid"
 XDC="${PROJECT_DIR}/build/bin/XDC"
-GENESIS_FILE="${WORK_DIR}/genesis/XDPoS-3-signers.json"
-
-touch .pwd
-mkdir -p "${LOG_DIR}"
-export $(cat .env | xargs)
-
 ENODE="enode://62457be5ca9c9ba3913d1513c22ca963b94548a7db06e7a629fec5b654ab7b09a704cba22229107b3f54848ae58e845dcce98393b48be619cc2860d56dd57198@127.0.0.1:30301"
-echo "ENODE = ${ENODE}"
+
+export $(cat .env | xargs)
+echo "ENODE=${ENODE}"
+
+function start_bootnode() {
+    echo -e "\nStarting the bootnode"
+    ${PROJECT_DIR}/build/bin/bootnode -nodekey bootnode.key --addr 0.0.0.0:30301 > /dev/null 2>&1 &
+    PID=$!
+    echo ${PID} > ${BOOTNODE_PID_FILE}
+    echo "bootnode is running now: ${PID}"
+}
+
+function start_node() {
+    NODE_ID=$1
+    NODE_NAME="pn${NODE_ID}"
+    echo -e "\nStarting the node ${NODE_ID}"
+
+    PRIVATE_KEY="PRIVATE_KEY_${NODE_ID}"
+    DATA_DIR="nodes/${NODE_NAME}"
+
+    if [ ! -d "${DATA_DIR}/XDC/chaindata" ]; then
+        WALLET=$(${XDC} account import --password .pwd --datadir ${DATA_DIR} <(echo ${!PRIVATE_KEY}) | awk -v FS="({|})" '{print $2}')
+        if [ ! -f genesis.json ]; then
+            cp genesis/XDPoS-3-signers.json genesis.json
+        fi
+        ${XDC} --datadir ${DATA_DIR} init genesis.json
+    else
+        WALLET=$(${XDC} account list --datadir ${DATA_DIR} | head -n 1 | awk -v FS="({|})" '{print $2}')
+    fi
+
+    echo "WALLET = ${WALLET}"
+
+    PORT=$((${BASE_PORT}+${NODE_ID}))
+    RPC_PORT=$((${BASE_RPC_PORT}+${NODE_ID}))
+    WS_RPC_PORT=$((${BASE_WS_RPC_PORT}+${NODE_ID}))
+
+    ${XDC} \
+        --mine \
+        --syncmode full \
+        --enable-0x-prefix \
+        --bootnodes ${ENODE} \
+        --datadir ${DATA_DIR} \
+        --networkid ${NETWORK_ID} \
+        --verbosity ${VERBOSITY} \
+        --gasprice ${GAS_PRICE} \
+        --targetgaslimit 420000000 \
+        --password .pwd \
+        --unlock "${WALLET}" \
+        --port ${PORT} \
+        --rpc \
+        --rpcaddr 0.0.0.0 \
+        --rpcport ${RPC_PORT} \
+        --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,XDPoS \
+        --rpccorsdomain "*" \
+        --rpcvhosts "*" \
+        --ws \
+        --wsaddr 0.0.0.0 \
+        --wsport ${WS_RPC_PORT} \
+        --wsorigins "*" \
+        > "${LOG_DIR}/${NODE_NAME}-$(date +%Y%m%d-%H%M%S).log" 2>&1 &
+
+    PID=$!
+    echo ${PID} > ${NODE_NAME}.pid
+    echo "node is running now: ${PID}"
+}
 
 cd ${PROJECT_DIR} && make all
 cd ${WORK_DIR}
 
-echo -e "\nStarting the bootnode"
-${PROJECT_DIR}/build/bin/bootnode -nodekey "${WORK_DIR}/bootnode.key" --addr 0.0.0.0:30301 > /dev/null 2>&1 &
-CHILD_PIDS=$!
+touch .pwd
+mkdir -p "${LOG_DIR}"
 
-for NODE_ID in {0..3} ; do
-  NODE_NAME="pn${NODE_ID}"
-  echo -e "\nStarting the node ${NODE_ID}"
+if [ -f ${BOOTNODE_PID_FILE} ]; then
+    PID=$(cat ${BOOTNODE_PID_FILE})
+        if [ -d "/proc/${PID}/fd" ]; then
+        echo "bootnode is already running: ${PID}"
+    else
+        start_bootnode
+    fi
+else
+    start_bootnode
+fi
 
-  PRIVATE_KEY="PRIVATE_KEY_${NODE_ID}"
-  DATA_DIR="${WORK_DIR}/nodes/${NODE_NAME}"
-
-  if [ ! -d "${DATA_DIR}/XDC/chaindata" ]; then
-    WALLET=$(${XDC} account import --password .pwd --datadir ${DATA_DIR} <(echo ${!PRIVATE_KEY}) | awk -v FS="({|})" '{print $2}')
-    ${XDC} --datadir ${DATA_DIR} init ${GENESIS_FILE}
-  else
-    WALLET=$(${XDC} account list --datadir ${DATA_DIR} | head -n 1 | awk -v FS="({|})" '{print $2}')
-  fi
-
-  echo "WALLET = ${WALLET}"
-  echo "PRIVATE_KEY = ${!PRIVATE_KEY}" 
-
-  PORT=$((${BASE_PORT}+${NODE_ID}))
-  RPC_PORT=$((${BASE_RPC_PORT}+${NODE_ID}))
-  WS_RPC_PORT=$((${BASE_WS_RPC_PORT}+${NODE_ID}))
-
-  ${XDC} \
-    --mine \
-    --syncmode full \
-    --enable-0x-prefix \
-    --bootnodes ${ENODE} \
-    --datadir ${DATA_DIR} \
-    --networkid ${NETWORK_ID} \
-    --verbosity ${VERBOSITY} \
-    --gasprice ${GAS_PRICE} \
-    --targetgaslimit 420000000 \
-    --password "${WORK_DIR}/.pwd" \
-    --unlock "${WALLET}" \
-    --port ${PORT} \
-    --rpc \
-    --rpcaddr 0.0.0.0 \
-    --rpcport ${RPC_PORT} \
-    --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,XDPoS \
-    --rpccorsdomain "*" \
-    --rpcvhosts "*" \
-    --ws \
-    --wsaddr 0.0.0.0 \
-    --wsport ${WS_RPC_PORT} \
-    --wsorigins "*" \
-    > "${LOG_DIR}/${NODE_NAME}-$(date +%Y%m%d-%H%M%S).log" 2>&1 &
-
-  CHILD_PIDS="${CHILD_PIDS} $!"
+for arg in $@; do
+    start_node ${arg}
 done
-
-echo ${CHILD_PIDS} > "${WORK_DIR}/private-networks.pid"
-
-echo -e "\nPIDs = ${CHILD_PIDS}\n"
-
--- stop-private-network.sh
+++ stop-private-network.sh
@@ -1,1 +1,37 @@
-kill $(cat private-networks.pid)
+#!/bin/bash
+set -eo pipefail
+
+if [ $# == 0 ] ; then
+    echo "Usage: $0 node_id ..."
+    echo "Examples:"
+    echo "$0 0"
+    echo "$0 2"
+    echo "$0 0 1"
+    echo "$0 2 3"
+    echo "$0 0 1 2"
+    echo "$0 0 1 2 3"
+    exit 1
+fi
+
+for arg in $@; do
+    if ! [ ${arg} -ge 0 ] 2>/dev/null ; then
+        echo "node_id ${arg} is not integer"
+        exit 2
+    fi
+done
+
+for arg in $@; do
+    PID_FILE="pn${arg}.pid"
+    if [ -f "${PID_FILE}" ]; then
+        PID=$(cat ${PID_FILE})
+            if [ -d "/proc/${PID}/fd" ]; then
+            kill ${PID}
+            echo "Stopping node pn${PID} ..."
+        else
+            echo "No such process: ${PID}"
+        fi
+        rm -f ${PID_FILE}
+    else
+        echo "PID file is not existing: ${PID_FILE}"
+    fi
+done

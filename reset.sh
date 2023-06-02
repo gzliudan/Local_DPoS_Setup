6a8aa8708ea1226316a4dcbf88ac5b16862d1048
Daniel Liu <liudaniel@qq.com>
2023-06-02 14:44:20 +0800

    delete data by wizard or flag in reset.sh

--- reset.sh
+++ reset.sh
@@ -2,6 +2,21 @@
 set -eo pipefail
 
 
+function help() {
+    echo
+    echo "About:"
+    echo "    This script delete directories nodes and logs under ${PWD}"
+    echo
+    echo "Usage:"
+    echo "    $0 [options]"
+    echo
+    echo "Options:"
+    echo "    -f, --force    delete data at once without confirmation"
+    echo "    -h, --help     display this help messages"
+    echo
+}
+
+
 function confirm() {
     read -p "$1 [Yes/No]: " yes_or_no
 
@@ -19,27 +34,52 @@ function confirm() {
 }
 
 
-echo "Which directory do you want to delete ?"
+function delete_all() {
+    echo "rm -rf nodes logs"
+    rm -rf nodes logs
+}
 
-PS3="Please select a number: "
-select DIR in "nodes logs" "nodes" "logs" "quit"; do
-    case ${DIR} in
-        "nodes logs"|"nodes"|"logs")
-            break
-            ;;
-		*)
-            echo "quit"
-			exit 1
-			;;
-	esac
-done    
 
-answer=$(confirm "Remove direcotry ${DIR} ?")
+function execute_wizard() {
+    echo "Do you want to delete directories nodes and logs ?"
 
-if [ "${answer}" = "Yes" ]; then
-    echo "rm -rf ${DIR}"
-    rm -rf ${DIR}
+    PS3="Please input a number: "
+    select choice in "Delete" "Quit"; do
+        case ${choice} in
+            "Delete" )
+                break
+                ;;
+            * )
+                echo "Quit"
+                return 1
+                ;;
+        esac
+    done    
+
+    answer=$(confirm "Do you want to delete all data ?")
+
+    if [ "${answer}" = "Yes" ]; then
+        delete_all
+    else
+        echo "Abort"
+        return 2
+    fi
+}
+
+
+if [ $# == 0 ] ; then
+    execute_wizard
+elif [ $# == 1 ] ; then
+    if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+        help
+        exit 0
+    elif [[ "$1" == "-f" || "$1" == "--force" ]]; then
+        delete_all
+    else
+        help
+        exit 3
+    fi
 else
-    echo "abort"
-    exit 2
+    help
+    exit 3
 fi
